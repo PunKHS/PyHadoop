c510d3d74ac4fd3ec3af9cc149d174b837c48833
PunKHS <stroitelev61@gmail.com>
2016-06-15 23:51:48 +0300

    Add example

---
+++ 3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+
+# Maper
+for line in sys.stdin:
+    for token in line.strip().split(" "):
+        if token: print(token + '\t1')
+
+# Reduser
+(lastKey, sum) = (None, 0)
+for line in sys.stdin:
+    (key, value) = line.strip().split("\t")
+    if lastKey and lastKey != key:
+        print(lastKey + '\t' + str(sum))
+        (lastKey, sum) = (key, int(value))
+    else:
+        (lastKey, sum) = (key, sum + int(value))
+if lastKey:
+    print(lastKey + '\t' + str(sum))
