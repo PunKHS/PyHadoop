d9cad00ea21da82a7fcda9e23a5094c906b679f1
PunKHS <stroitelev61@gmail.com>
2016-06-16 14:26:13 +0300

    4.2

---
+++ 4.2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+
+dic = {}
+# for line in open('text_4.2.txt', 'r'):
+for line in sys.stdin:
+
+    words = line.strip().split()
+    for word in words:
+        (key, value) = (word, word.count(word))
+        if key in dic:
+            dic[key] += 1
+        else:
+            dic[key] = value
+for i in dic:
+    print(i + '\t' + str(dic[i]))
